f2859aae3689127397ae8dfca82c381254410abb
Olga Gogoleva <mikadoolga@yandex.ru>
2024-06-15 16:53:24 +0300

    Init

---
+++ src/main/java/Post.java
@@ -0,0 +1,10 @@
+public class Post {
+    public String name;          // поле для имени
+    public String passport;      // номер паспорта
+    public String patronymic;    // отчество
+    public String phone;         // номер телефона
+    public String surname;       // Фамилия
+    public boolean subscription; //  зарегистрирован?
+    public FormDate birthday;
+
+}
